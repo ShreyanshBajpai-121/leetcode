8756c665e41f3692b99f69ec1114596decaeb91d
ShreyanshBajpai-121 <tech.shreyansh45@gmail.com>
2025-11-16 00:28:39 +0530

    Time: 0 ms (100%), Space: 53 MB (9.91%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int n=arr.length;
+        if(n==1) return arr[0];
+        if (arr[0] != arr[1]) return arr[0];
+
+       else  if(arr[n-1]!=arr[n-2]) return arr[n-1];
+        else{
+            int st=1;
+            int end=n-2;
+            while(st<=end){
+                int mid=st+(end-st)/2;
+                if(arr[mid]!=arr[mid-1]&&arr[mid]!=arr[mid+1]) return arr[mid];
+                if((mid%2==1 &&arr[mid]==arr[mid-1])||(mid%2==0&&arr[mid]==arr[mid+1]))
+                st=mid+1;
+                else
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+}
