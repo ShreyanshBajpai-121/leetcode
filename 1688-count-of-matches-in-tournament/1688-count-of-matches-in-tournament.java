281c07e011ca32fcbe79c541990b17d1059aaff3
ShreyanshBajpai-121 <tech.shreyansh45@gmail.com>
2026-05-02 13:33:50 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.21%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfMatches(int n) {
+      return n-1;
+    //    OR
+     // int match=0;
+        // while(n>1){
+        //     if(n%2!=0){
+        //         match+=(n-1)/2;
+        //         n=(n-1)/2+1;
+        //     }else{
+        //         match+=n/2;
+        //         n=n/2;
+        //     }
+        // }
+        // return match;
+    }
+}
