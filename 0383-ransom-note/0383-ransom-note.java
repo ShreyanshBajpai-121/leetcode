639a7279ca0d33e2e0bc393e70eea788ede6f1cd
ShreyanshBajpai-121 <tech.shreyansh45@gmail.com>
2026-06-08 21:54:39 +0530

    Time: 1 ms (99.94%), Space: 46.2 MB (89.11%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int [] arr=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            arr[ch-'a']++;
+        }
+          for(int i=0;i<ransomNote.length();i++){
+            char ch=ransomNote.charAt(i);
+            arr[ch-'a']--;
+            if(arr[ch-'a']<0) return false;
+        }
+        return true;
+    }
+}
