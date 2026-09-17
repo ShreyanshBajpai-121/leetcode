a07f7a72f380011de3f7d114dc9e474517c3288c
ShreyanshBajpai-121 <tech.shreyansh45@gmail.com>
2026-09-17 23:01:52 +0530

    Time: 5 ms (82.86%), Space: 77.6 MB (12.84%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxArea(int[] height) {
       int max=Integer.MIN_VALUE;
-     int left=0;
+      int left=0;
      int right=height.length-1;
      while(left<right){
         int w=right-left;
@@ -9,7 +9,7 @@ class Solution {
         max=Math.max(max,(w*h));
         if(height[left]>height[right])right--;
         else left++;
-     }
-      return max;  
+    }
+    return max;
     }
 }
